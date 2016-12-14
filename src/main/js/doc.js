sandflow/imscJS

commit 1e4d7256710c43bef6eb5f1da2f806466c2dc360

Fixed line padding

src/main/js/doc.js
@@ -83,11 +83,9 @@
 
                 // remove redundant nested anonymous spans (9.3.3(1)(c))
 
-                if (estack[0] instanceof Span &&
-                        estack[0].contents.length === 1 &&
+                if (estack[0].contents.length === 1 &&
                         estack[0].contents[0] instanceof Span &&
-                        estack[0].contents[0].anon &&
-                        estack[0].text === null) {
+                        estack[0].contents[0].anon) {
 
                     estack[0].text = estack[0].contents[0].text;
                     estack[0].contents = [];

src/main/js/html.js
@@ -183,35 +183,6 @@
 
                 }
 
-
-
-                // clean-up the span
-
-                /*var child = e.firstChild;
-                 
-                 var acctext = "";
-                 
-                 while (child) {
-                 
-                 var cur_child = child;
-                 
-                 child = child.nextSibling;
-                 
-                 if (cur_child.nodeName === "br" || child === null) {
-                 
-                 e.insertBefore(document.createTextNode(acctext), cur_child);
-                 
-                 acctext = "";
-                 
-                 } else {
-                 
-                 acctext += cur_child.textContent;
-                 
-                 e.removeChild(cur_child);
-                 
-                 }
-                 
-                 }*/
             } else {
                 e.textContent = isd_element.text;
             }
@@ -232,88 +203,181 @@
 
         if ((context.lp || context.mra) && isd_element.kind === "p") {
 
-            // insert explicit line breaks for multirow align and line padding
+            var elist = [];
 
-            insertExplicitBrAndPadding(proc_e, context.lp, null);
+            constructElementList(proc_e, elist, "red");
 
-            if (context.lp) delete context.lp;
-            if (context.mra) delete context.mra;
+            /* TODO: linePadding only supported for horizontal scripts */
+
+            processLinePaddingAndMultiRowAlign(elist, context.lp * context.h);
+
+            /* TODO: clean-up the span */
+
+            // clean-up the span
+
+            /*var child = e.firstChild;
+             
+             var acctext = "";
+             
+             while (child) {
+             
+             var cur_child = child;
+             
+             child = child.nextSibling;
+             
+             if (cur_child.nodeName === "br" || child === null) {
+             
+             e.insertBefore(document.createTextNode(acctext), cur_child);
+             
+             acctext = "";
+             
+             } else {
+             
+             acctext += cur_child.textContent;
+             
+             e.removeChild(cur_child);
+             
+             }
+             
+             }*/
+
+            if (context.lp)
+                delete context.lp;
+            if (context.mra)
+                delete context.mra;
 
         }
 
     }
 
-    function insertExplicitBrAndPadding(e, lp, boundingrecttop) {
+    function constructElementList(element, elist, bgcolor) {
 
-        var child = e.firstChild;
+        if (element.childElementCount === 0) {
 
-        var last_top = boundingrecttop;
+            elist.push({"element": element, "bgcolor": bgcolor});
 
-        while (child) {
+        } else {
 
-            if (child.nodeType === Node.ELEMENT_NODE) {
+            var newbgcolor = element.style.backgroundColor || bgcolor;
 
-                var cur_top = child.getBoundingClientRect().top;
+            var child = element.firstChild;
 
-                // has the top of the bounding rect changed
-                // TODO: this fails on vertical scripts
+            while (child) {
 
-                if (cur_top !== last_top && child.localName !== "br") {
+                if (child.nodeType === Node.ELEMENT_NODE) {
 
-                    // apparently bounding rects for <br> are not the same as other inline elements, so skip it
-
-                    if (child.previousSibling !== null && child.previousSibling.localName !== "br") {
-
-                        e.insertBefore(document.createElement("br"), child);
-
-                        // rewind to handle line padding
-
-                        child = child.previousSibling.previousSibling;
-
-                    } else {
-
-                        last_top = cur_top;
-
-                    }
+                    constructElementList(child, elist, newbgcolor);
 
                 }
 
-                if (lp && lp > 0) {
-                    
-                     var s;
-
-                    if ((child.previousSibling === null && boundingrecttop === null) ||
-                            (child.previousSibling !== null && child.previousSibling.localName === "br")) {
-
-                        s = "-0.5em 0 " + getComputedStyle(child).backgroundColor;
-
-                        if (child.style.boxShadow)
-                            s += "," + child.style.boxShadow;
-
-                        child.style.boxShadow = s;
-
-                    }
-
-                    if ((boundingrecttop === null && child.nextSibling === null) ||
-                            (child.nextSibling !== null && child.nextSibling.localName === "br")) {
-
-                        s = "0.5em 0 " + getComputedStyle(child).backgroundColor;
-
-                        if (child.style.boxShadow)
-                            s += "," + child.style.boxShadow;
-
-                        child.style.boxShadow = s;
-                    }
-
-                }
-
-                insertExplicitBrAndPadding(child, lp, last_top);
+                child = child.nextSibling;
             }
+        }
 
-            child = child.nextSibling;
+    }
+
+    function processLinePaddingAndMultiRowAlign(elist, lp) {
+
+        var line_head = null;
+        
+        var lookingForHead = true;
+
+        for (var i = 0; i <= elist.length; i++) {
+
+            /* skip <br> since they apparently have a different box top than
+             * the rest of the line 
+             */
+
+            if (i !== elist.length && elist[i].element.localName === "br")
+                continue;
+            
+            /* detect new line */
+
+            if (line_head === null ||
+                    i === elist.length ||
+                    elist[i].element.getBoundingClientRect().top !== elist[line_head].element.getBoundingClientRect().top) {
+                              
+                /* apply right padding to previous line (if applicable and unless this is the first line) */
+
+                if (lp && (!lookingForHead)) {
+
+                    for (; --i >= 0;) {
+
+                        if (elist[i].element.getBoundingClientRect().width !== 0) {
+
+                            addRightPadding(elist[i].element, elist[i].color, lp);
+
+                            if (elist[i].element.getBoundingClientRect().width !== 0 && 
+                                    elist[i].element.getBoundingClientRect().top === elist[line_head].element.getBoundingClientRect().top)
+                                break;
+
+                            removeRightPadding(elist[i].element);
+
+                        }
+
+                    }
+                    
+                    lookingForHead = true;
+                    
+                    continue;
+
+                }
+                
+                /* explicit <br> unless already present */
+
+                if (i !== elist.length && line_head !== null && elist[i-1].element.localName !== "br") {
+
+                    var br = document.createElement("br");
+
+                    elist[i].element.parentElement.insertBefore(br, elist[i].element);
+
+                    elist.splice(i, 0, {"element": br});
+                    
+                    continue;
+
+                }
+                
+                /* apply left padding to current line (if applicable) */
+                
+                if (i !== elist.length && lp) {
+                    
+                    /* find first non-zero */
+                    
+                    for (; i < elist.length; i++) {
+
+                        if (elist[i].element.getBoundingClientRect().width !== 0) {
+                            break;
+                        }
+                        
+                    }
+                    
+                    addLeftPadding(elist[i].element, elist[i].color, lp);
+                
+                }
+                
+                lookingForHead = false;
+                
+                line_head = i;
+
+            }
 
         }
 
+    }
+
+    function addLeftPadding(e, c, lp) {
+        e.style.paddingLeft = lp + "px";
+        e.style.backgroundColor = c;
+    }
+
+    function addRightPadding(e, c, lp) {
+        e.style.paddingRight = lp + "px";
+        e.style.backgroundColor = c;
+
+    }
+
+    function removeRightPadding(e) {
+        e.style.paddingRight = null;
     }
 
 
@@ -552,28 +616,28 @@
         new HTMLStylingMapDefintion(
                 "http://www.w3.org/ns/ttml#styling textAlign",
                 function (context, dom_element, isd_element, attr) {
-                    
+
                     var ta;
                     var dir = isd_element.styleAttrs[imscStyles.byName.direction.qname];
-                    
+
                     /* handle UAs that do not understand start or end */
-                    
+
                     if (attr === "start") {
-                                                   
+
                         ta = dir === "ltr" ? "left" : "right";
- 
+
                     } else if (attr === "end") {
-                        
+
                         ta = dir === "ltr" ? "right" : "left";
-                        
+
                     } else {
-                        
+
                         ta = attr;
-                        
+
                     }
-                    
+
                     dom_element.style.textAlign = ta;
-                    
+
                 }
         ),
         new HTMLStylingMapDefintion(

commit 60f394a28225bbc9c05afe1d52df848cad024d53

Fixed bug introduced in 1e4d7256710c43bef6eb5f1da2f806466c2dc360

src/main/js/doc.js
@@ -91,9 +91,11 @@
 
                 // remove redundant nested anonymous spans (9.3.3(1)(c))
 
-                if (estack[0].contents.length === 1 &&
+                if (estack[0] instanceof Span &&
+                        estack[0].contents.length === 1 &&
                         estack[0].contents[0] instanceof Span &&
-                        estack[0].contents[0].anon) {
+                        estack[0].contents[0].anon &&
+                        estack[0].text === null) {
 
                     estack[0].text = estack[0].contents[0].text;
                     estack[0].contents = [];