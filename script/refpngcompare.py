b1086e4f64d739da375ac2c8c76fbebe09d43a83
Pierre-Anthony Lemieux <pal@palemieux.com>
2017-07-17 16:39:23 +0200

    PNG compare script can now output visual diffs

--- script/refpngcompare.py
+++ script/refpngcompare.py
@@ -11,6 +11,8 @@ parser.add_argument("ref_dir", help = "Path of the Reference directory")
 
 parser.add_argument("render_dir", help = "Path of the Generated directory")
 
+parser.add_argument("-d", help = "Specifies whether a diff of images is output")
+
 args = parser.parse_args()
 
 for dir, dirs, files in os.walk(args.ref_dir):
@@ -31,6 +33,17 @@ for dir, dirs, files in os.walk(args.ref_dir):
                 r = float(m.group(2))
                 if (r > 0.0001) :
                     print(reffile + ": " + str(r))
+                    if args.d is not None:
+                      diffdir = args.d
+                      #diffdir = os.path.join(args.d, os.path.relpath(dir, args.ref_dir))
+                      if not os.path.exists(diffdir):
+                        os.makedirs(diffdir)
+                      difffile = os.path.join(args.d, os.path.relpath(dir, args.ref_dir) + "-" + file)
+                      p1 = subprocess.Popen(["compare", reffile, genfile, "png:-"], stdout=subprocess.PIPE)
+                      p2 = subprocess.Popen(["montage", "-mode", "concatenate", reffile, "-", genfile, difffile], stdin=p1.stdout)
+                      p1.stdout.close()
+                      p2.communicate()
+
 
 #        elif not fnmatch.fnmatch(file, 'manifest.json'):
 #            try:
