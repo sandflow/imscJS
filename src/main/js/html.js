92f9507661e40ba028b854b6530b72e779454feb
Pierre-Anthony Lemieux <pal@sandflow.com>
2018-12-22 14:35:12 -0800

    ebutts:linePadding emulated using border instead of padding (#137)

--- src/main/js/html.js
+++ src/main/js/html.js
@@ -247,15 +247,17 @@
 
             /* apply padding to the <p> so that line padding does not cause line wraps */
 
+            var padmeasure = Math.ceil(lp * context.h) + "px";
+
             if (context.bpd === "tb") {
 
-                proc_e.style.paddingLeft = lp * context.h + "px";
-                proc_e.style.paddingRight = lp * context.h + "px";
+                proc_e.style.paddingLeft = padmeasure;
+                proc_e.style.paddingRight = padmeasure;
 
             } else {
 
-                proc_e.style.paddingTop = lp * context.h + "px";
-                proc_e.style.paddingBottom = lp * context.h + "px";
+                proc_e.style.paddingTop = padmeasure;
+                proc_e.style.paddingBottom = padmeasure;
 
             }
 
@@ -429,45 +431,57 @@
 
             var ee = lineList[i].elements[lineList[i].end_elem];
 
+            var pospadpxlen = Math.ceil(lp) + "px";
+
+            var negpadpxlen = "-" + Math.ceil(lp) + "px";
+
             if (l !== 0) {
 
                 if (context.ipd === "lr") {
 
-                    se.node.style.paddingLeft = lp + "px";
-                    se.node.style.marginLeft = "-" + lp + "px";
+                    se.node.style.borderLeftColor = se.bgcolor || "#00000000";
+                    se.node.style.borderLeftStyle = "solid";
+                    se.node.style.borderLeftWidth = pospadpxlen;
+                    se.node.style.marginLeft = negpadpxlen;
 
                 } else if (context.ipd === "rl") {
 
-                    se.node.style.paddingRight = lp + "px";
-                    se.node.style.marginRight = "-" + lp + "px";
+                    se.node.style.borderRightColor = se.bgcolor || "#00000000";
+                    se.node.style.borderRightStyle = "solid";
+                    se.node.style.borderRightWidth = pospadpxlen;
+                    se.node.style.marginRight = negpadpxlen;
 
                 } else if (context.ipd === "tb") {
 
-                    se.node.style.paddingTop = lp + "px";
-                    se.node.style.marginTop = "-" + lp + "px";
+                    se.node.style.borderTopColor = se.bgcolor || "#00000000";
+                    se.node.style.borderTopStyle = "solid";
+                    se.node.style.borderTopWidth = pospadpxlen;
+                    se.node.style.marginTop = negpadpxlen;
 
                 }
-
-                se.node.style.backgroundColor = se.bgcolor;
 
                 if (context.ipd === "lr") {
 
-                    ee.node.style.paddingRight = lp + "px";
-                    ee.node.style.marginRight = "-" + lp + "px";
+                    ee.node.style.borderRightColor = ee.bgcolor  || "#00000000";
+                    ee.node.style.borderRightStyle = "solid";
+                    ee.node.style.borderRightWidth = pospadpxlen;
+                    ee.node.style.marginRight = negpadpxlen;
 
                 } else if (context.ipd === "rl") {
 
-                    ee.node.style.paddingLeft = lp + "px";
-                    ee.node.style.marginLeft = "-" + lp + "px";
+                    ee.node.style.borderLeftColor = ee.bgcolor || "#00000000";
+                    ee.node.style.borderLeftStyle = "solid";
+                    ee.node.style.borderLeftWidth = pospadpxlen;
+                    ee.node.style.marginLeft = negpadpxlen;
 
                 } else if (context.ipd === "tb") {
 
-                    ee.node.style.paddingBottom = lp + "px";
-                    ee.node.style.marginBottom = "-" + lp + "px";
+                    ee.node.style.borderBottomColor = ee.bgcolor || "#00000000";
+                    ee.node.style.borderBottomStyle = "solid";
+                    ee.node.style.borderBottomWidth = pospadpxlen;
+                    ee.node.style.marginBottom = negpadpxlen;
 
                 }
-
-                ee.node.style.backgroundColor = ee.bgcolor;
 
             }
 
